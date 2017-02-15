0fdcf5bd294d5d37a37be86230c4cfe8eb41dcc1
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-02-15 17:33:56 -0500

    Completed Lab, No Advanced Portion

--- CollectionViewCatPicPuzzle/CollectionViewController.swift
+++ CollectionViewCatPicPuzzle/CollectionViewController.swift
@@ -7,25 +7,119 @@
 //
 
 import UIKit
+import GameKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var headerReusableView: HeaderReusableView!
+    var footerReusableView: FooterReusableView!
+    var imageSlices: [UIImage] = []
+    
+    var sectionInsets: UIEdgeInsets!
+    var spacing: CGFloat!
+    var itemSize: CGSize!
+    var referenceSize: CGSize!
+    var numberOfRows: CGFloat!
+    var numberOfColumns: CGFloat!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureLayout()
+        self.collectionView?.register(HeaderReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+        self.collectionView?.register(FooterReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footer")
+        
+        for i in 1...12 {
+            imageSlices.append(UIImage(named: "\(i)")!)
+        }
+        randomize()
+        
+   
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return imageSlices.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = self.collectionView?.dequeueReusableCell(withReuseIdentifier: "puzzleCell", for: indexPath) as! CollectionViewCell
         
-        cell.imageView.image = UIImage(named: "cats")
+        cell.imageView.image = imageSlices[indexPath.item]
         
         return cell
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionElementKindSectionHeader {
+            
+            headerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)) as! HeaderReusableView
+            
+            return headerReusableView
+            
+        } else {
+            
+            footerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)) as! FooterReusableView
+            
+            return footerReusableView
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    }
+    
+    func configureLayout() {
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        
+        
+        numberOfRows = 4
+        numberOfColumns = 3
+        spacing = 2
+        sectionInsets = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        referenceSize = CGSize(width: width, height: 60)
+        
+        
+        let headerHeight = referenceSize.height
+        let footerHeight = referenceSize.height
+
+        let itemWidth = (width - ((numberOfColumns - 1)*(spacing) + sectionInsets.left + sectionInsets.right))/numberOfColumns
+        let itemHeight = (height - ((numberOfRows - 1)*(spacing) + sectionInsets.top + sectionInsets.bottom + headerHeight + footerHeight))/numberOfRows
+        
+        itemSize = CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    func randomize() -> [UIImage] {
+        imageSlices = GKRandomSource().arrayByShufflingObjects(in: imageSlices) as! [UIImage]
+        return imageSlices
         
     }
     
